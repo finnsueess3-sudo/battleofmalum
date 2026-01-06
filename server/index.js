cd8fd8f5dc1535dbde9f4b0b292b80fbfe55d168
finnsueess3-sudo <finn.sueess3@gmail.com>
2026-01-06 12:53:10 +0100

    Update player initialization and attack logic

--- server/index.js
+++ server/index.js
@@ -9,30 +9,54 @@ let players = {};
 
 io.on("connection", socket => {
   players[socket.id] = {
-    x: 400,
-    y: 300,
+    id: socket.id,
+    x: Math.random() * 700 + 50,
+    y: Math.random() * 500 + 50,
     hp: 100,
     weapon: "sword"
   };
 
   socket.emit("init", players[socket.id]);
-  io.emit("players", players);
+  io.emit("update", players);
 
   socket.on("move", data => {
-    if (players[socket.id]) {
-      players[socket.id].x = data.x;
-      players[socket.id].y = data.y;
-      io.emit("players", players);
-    }
+    if (!players[socket.id]) return;
+    players[socket.id].x = data.x;
+    players[socket.id].y = data.y;
+    io.emit("update", players);
   });
 
-  socket.on("attack", () => {
-    io.emit("attack", socket.id);
+  socket.on("attack", data => {
+    for (let id in players) {
+      if (id === socket.id) continue;
+
+      let dx = players[id].x - data.x;
+      let dy = players[id].y - data.y;
+      let dist = Math.sqrt(dx*dx + dy*dy);
+
+      if (
+        (data.weapon === "sword" && dist < 40) ||
+        (data.weapon === "gun" && dist < 200)
+      ) {
+        players[id].hp -= data.weapon === "sword" ? 15 : 8;
+        if (players[id].hp <= 0) {
+          players[id].hp = 100;
+          players[id].x = Math.random() * 700 + 50;
+          players[id].y = Math.random() * 500 + 50;
+        }
+      }
+    }
+    io.emit("update", players);
+    io.emit("blood", data);
+  });
+
+  socket.on("weapon", w => {
+    if (players[socket.id]) players[socket.id].weapon = w;
   });
 
   socket.on("disconnect", () => {
     delete players[socket.id];
-    io.emit("players", players);
+    io.emit("update", players);
   });
 });
 
